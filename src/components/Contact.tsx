49030e83c5483ee671726107eee701f437eba5ae
prathmeshshinde <prathmeshshinde105@gmail.com>
2025-06-13 09:48:20 +0530

    Update Google Drive link in Contact component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -149,7 +149,7 @@ const Contact = () => {
             </div>
             <Button asChild className="gap-2">
               <a
-                href="https://drive.google.com/file/d/1Gn_l7jFGegRPUasJSsYlyAO9OKEH7XX5/view?usp=sharing"
+                href="https://drive.google.com/file/d/1mZuLK3ZxiaQiAVNCXGI_HaQ0JSgXfPEQ/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
               >
